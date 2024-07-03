9daca46a651abf8fdbd5d2a075ff4ce9c10e7b15
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2024-07-03 23:10:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,45 @@ let getMovie = () => {
         results.innerHTML = `<h3 class="msg">Please enter a movie or tv show </h3>`;
     }
 
-}
+    else {
+        fetch(url).then((resp) => resp.json()).then((data) => {
+            
+            if (data.Response == "True") {
+                results.innerHTML = `
+                    <div class="info">
+                        <img src=${data.Poster} class="poster">
+                        <div>
+                            <h2>${data.Title}</h2>
+                            <div class="rating">
+                                <img src="star.svg">
+                                <h4>${data.imdbRating}</h4>
+                            </div>
+                            <div class="details">
+                                <span>${data.Rated}</span>
+                                <span>${data.Year}</span>
+                                <span>${data.Runtime}</span>
+                            </div>
+                            <div class="genre">
+                                <div>${data.Genre.split(",").join("</div><div>")}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <h3>Plot:</h3>
+                    <p>${data.Plot}</p>
+                    <h3>Cast:</h3>
+                    <p>${data.Actors}</p>
+                `;
+            }
+
+            else {
+                results.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+            }
+        })
+            .catch(() => {
+                results.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+            });
+    }
+};
+
+search_button.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
