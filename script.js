7ddc8582615430d6a26a7d07f8b2968a0dc59ff4
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2024-07-03 23:04:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ let results = document.getElementById("results");
 let getMovie = () => {
     let movieName = movieNameRef.value;
     let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+
+    if(movieName.length <= 0){
+        results.innerHTML = `<h3 class="msg">Please enter a movie or tv show </h3>`;
+    }
+
 }
